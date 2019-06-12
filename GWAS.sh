62da9a60afc5a1ec954b125cb27d666f41d094e1
Jill Marzolino <jmarz001@ucr.edu>
2019-06-12 14:59:47 -0700

    filter VCF file for minor allele freq and missingness

--- GWAS.sh
+++ GWAS.sh
@@ -8,3 +8,34 @@
 #SBATCH --mem=64gb
 #SBATCH -t 2-00:00:00
 #SBATCH -p koeniglab
+
+DIR=/rkett001/bigdata/row_gwas
+mkdir $DIR
+# set your VCF file
+VCF=../../data/SNPs/africa_and1001.EVA_filtered.vcf.gz
+# copy VCF file to new project directory
+cp $VCF $DIR
+# now move to working directory
+cd $DIR
+module load plink/1.90b3.38
+## Filter on MAF and missing data
+plink --vcf "$VCF" --maf 0.01 --geno 0.05 --make-bed --out $DIR/filtered
+
+
+# determine which seqs to use as phenotypes
+Rscript 3b_prep_phenotypes.R
+
+# add phenotypes to .fam
+PHENOS=../../results/gwas/phenotypes_transformed.txt
+GENOS=../../results/gwas/filtered.fam
+
+# check that IDs in phenos and genos match
+DIFS=$(diff <(cut -d" " -f1 "$GENOS") <(cut -f1 "$PHENOS" | tail -n +2) | wc -l)
+
+# no differences between ID lists
+if [ "$DIFS" = 0 ]
+then
+	paste  <(cut -d" " -f1-5 "$GENOS") <(cut -f3- "$PHENOS" | tail -n +2) | sed 's/\t/ /g' > $(basename $GENOS).tmp
+	mv $(basename $GENOS).tmp "$GENOS"
+	head -n 1 "$PHENOS" | cut -f3- | sed 's/\t/\n/g' > ../../results/gwas/pheno_lst.txt
+fi
