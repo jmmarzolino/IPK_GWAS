74677d88241d9385b900e09b743e33b34a87183d
Jill Marzolino <jmarz001@ucr.edu>
2019-06-13 09:47:15 -0700

    messed around a lot but the good stuff is in there. needs polish

--- GWAS.sh
+++ GWAS.sh
@@ -12,8 +12,12 @@ cd $DIR
 VCF=Barley_exome_GH_LD_prunded.recode.vcf
 module load plink/1.90b3.38
 ## Filter on MAF and missing data
-plink --vcf "$VCF" --allow-extra-chr --maf 0.01 --geno 0.05 --make-bed --out $DIR/filtered
+plink --vcf "$VCF" --allow-extra-chr --maf 0.01 --geno 0.05 --out $DIR/filtered
 
+module load vcftools
+vcftools --vcf Barley_exome_GH_LD_prunded.recode.vcf --remove-indels --recode --recode-INFO-all --out Barley_exome_GH_LD_prunded2
+
+plink --vcf "$VCF" --allow-extra-chr --maf 0.01 --geno 0.05 --recode bimbam --out $DIR/filtered2
 
 # format pheotype file
 cut -f3 Phenotype_GWAS > Accession_Codes
@@ -40,7 +44,26 @@ DIFS=$(diff <(cut -d" " -f1 "$GENOS") <(cut -f1 "$PHENOS" | tail -n +2) | wc -l)
 # delete non-common entries:  ERR753224 from genotype data & ERR753318 from phenotype data
 
 # no differences between ID lists
-if [ "$DIFS" = 0 ]
-then
-	paste  <(cut -d" " -f1-5 "$GENOS") <(cut -f3- "$PHENOS" | tail -n +2) | sed 's/\t/ /g' > $(basename $GENOS)
-fi
+paste  <(cut -d" " -f1-5 Barley_exome_GH_LD_INDEL_MAF.fam) <(cut -f6 binary) |  > $GENOS
+
+
+
+#!/bin/bash
+#SBATCH --job-name=GWAS
+#SBATCH -o GWAS3.stdout
+#SBATCH --ntasks=2
+#SBATCH --mem=64gb
+#SBATCH -t 2-00:00:00
+#SBATCH -p koeniglab
+
+cd /rhome/rkett/bigdata/row_gwas
+# calculate centered relatedness matrix
+/rhome/rkett/software/gemma0.98.1 -bfile filtered -gk 1 -outdir "results" -o related_matrix
+
+
+# gwa with mlm
+#/rhome/rkett/software/gemma0.98.1 -g Barley_exome_GH_LD_INDEL_MAF -k ./results/related_matrix_binary.cXX.txt -lmm 4 -o row_assoc
+
+
+# association with mlm
+/rhome/rkett/software/gemma0.98.1 -bfile Barley_exome_GH_LD_INDEL_MAF -k results/related_matrix_binary.cXX.txt -lmm 4 -outdir results/ -o row_assoc
