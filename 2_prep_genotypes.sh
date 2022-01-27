bdf87b4a5e46c7f1627459dfe07b350bfac508ff
Jill Marzolino <jmarz001@ucr.edu>
2022-01-27 12:39:04 -0800

    notes and rm LD filter - this set has already been pruned

--- 2_prep_genotypes.sh
+++ 2_prep_genotypes.sh
@@ -9,21 +9,19 @@
 # set directories
 PROJECT_DIR=/rhome/jmarz001/bigdata/IPK_Analysis/results/GWAS/
 cd $PROJECT_DIR
+
+#plink 2.0 is not yet fully usable on its own so use 1.90
 module load plink/1.90b3.38
 
 ## Filter on MAF and missing data  
 plink --vcf "$VCF" --maf 0.01 --geno 0.05 --make-bed --out ../../results/gwas/filtered
+# all the sequencing files (separated and parsed?) are listed here
+#/rhome/jmarz001/shared/SEQ_RUNS/FULL_RAD_RECORDS/RECORDS.txt
 
-### OLD CODE BELOW ###
-# LD pruning
-# identify SNPs to prune
-#plink --bfile filtered --indep-pairwise 50 10 0.9 --out ldprune
 # The resulting SNP vcf file consists of 74,231 SNPs for 192 individuals.
 # was LD pruned, but other filters unknown, may have been applied to JBLs input: FINAL.vcf which was filtered by Dan?
 VCF=/rhome/jlandis/bigdata/RADSeq/IPK/VCFtools/IPK_LD_pruned_SNPs.recode.vcf
 # another file in the dir, just progeny?
 # IPK_SNP_set_progeny.vcf.recode.vcf.gz
 
-# extract surviving SNPs 
-#plink --bfile filtered --extract ldprune.prune.in --make-bed --out filtered2
 
